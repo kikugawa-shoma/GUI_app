5abf1a77f037089f4f939631cb45b9f80892d7ac
kikugawa-shoma <kikugawa-s@sys.i.kyoto-u.ac.jp>
2020-08-04 20:41:05 +0900

    [proto]システム部分は完成。
    todo:GUIの作成

--- CP.py
+++ CP.py
@@ -1,27 +1,33 @@
 import pandas as pd
+from collections import defaultdict
 
 class pokemon():
-    def __init__(self,poke_name):
-        #!!各値はデータベースから引っ張ってくる
-        self.A = 263
-        self.D = 198
-        self.H = 209
+    def __init__(self,poke_name,Aiv,Div,Hiv,PL):
+        #各値はファイルから引っ張ってくる
+        self.A = df.loc[poke_name,"ATK"]
+        self.D = df.loc[poke_name,"DEF"]
+        self.H = df.loc[poke_name,"HP"]
         #!!ポケモンレベルと各個体値は入力から持ってくる
-        self.PL = 36
-        self.Aiv = 15
-        self.Div = 15
-        self.Hiv = 15
+        self.PL = PL
+        self.Aiv = Aiv
+        self.Div = Div
+        self.Hiv = Hiv
     def CP(self):
         #!!self.cprはCP補正値
-        self.cpr = 0.5888984165
+        self.cpr = cprs[self.PL]
         return int((self.A+self.Aiv)*(self.D+self.Div)**0.5*(self.H+self.Hiv)**0.5*self.cpr/10)
 
-
 df = pd.read_pickle(r"C:\Users\ktmks\programming\GUI_app\Data\race_values.pkl")
+with open(r"C:\Users\ktmks\programming\GUI_app\Data\cpr.txt") as f:
+    cprs_data = f.read().split("\n")
+cprs = defaultdict(lambda:-1)
+for i in range(len(cprs_data)):
+    pl, r = map(float,cprs_data[i].split("\t"))
+    cprs[pl] = r
 
 
 
 
 if __name__ == "__main__":
-    kairyu = pokemon("kairyu")
+    kairyu = pokemon("カイリュー",15,15,15,35.5)
     print(kairyu.CP())
